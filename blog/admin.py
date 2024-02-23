74322e4f11e1f4aeff59c61439c6ce8921fcfa20
Joseph Mazzei <j.mazzei.pv@gmail.com>
2024-02-23 17:04:43 +0000

    Created a few users and added commented notes for myself

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+# The .period infront of models is to indicate that models is in the same directory
+# as this admin.py file
 from .models import Post
 
 # Register your models here.
