ccfff641f1fd31630904140536e8ff8ee3163046
Joseph Mazzei <j.mazzei.pv@gmail.com>
2024-02-24 10:37:38 +0000

    Added admin rights to comments and tested creation

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 # The .period infront of models is to indicate that models is in the same directory
 # as this admin.py file
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(Comment)
