d0c26ec5507a851dc6656064fc7c50211b6c2085
Joseph Mazzei <j.mazzei.pv@gmail.com>
2024-02-24 10:54:44 +0000

    Added meta class for ordering, str dunder for super user UX

--- blog/models.py
+++ blog/models.py
@@ -26,10 +26,27 @@ class Post(models.Model):
     # A draft is defined as zero and published as one, so you can see the default is to save as a draft.
     status = models.IntegerField(choices=STATUS, default=0)
 
+    class Meta:
+        # - =Descending, most recent at the top, ? =Randomized, blank =Ascending, oldest first
+        # First descends through created_on, then ascends that list in author
+        ordering = ["-created_on", "author"]
+        
+    def __str__(self):
+        # Returns the title of post in the string instead of "PostObject1"
+        return f"{self.title} | Written by {self.author}"
+
 
 class Comment(models.Model):
+    # Post and Author are a 1-to-many relation, 1 user can have multiple posts/comments,
+    # so they must be Foreign Keys. On delete of post or account, all comments will be deleted.
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment: {self.body} by {self.author}"
