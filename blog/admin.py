6716fc80581d0d1fd30032e9c6e0e5a5f8ce1795
Joseph Mazzei <j.mazzei.pv@gmail.com>
2024-02-25 19:50:58 +0000

    Added Summernote text editor

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,19 @@ from django.contrib import admin
 # The .period infront of models is to indicate that models is in the same directory
 # as this admin.py file
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+
+# This code gives admin panel greater functionality and clarity
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
 
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Comment)
