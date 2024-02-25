0238ea4f49b35f3bc0d6eff755565f870fd5b74b
Joseph Mazzei <j.mazzei.pv@gmail.com>
2024-02-25 17:23:47 +0000

    Filters applied to home

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,6 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
+    # Filters to only display published blogs on home page, not drafts
+    queryset = Post.objects.all().order_by("-created_on").filter(status=1)
     template_name = "post_list.html"
