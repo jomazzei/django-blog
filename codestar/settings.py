1dcc1ba28a514c256c5b96b15f0162e4edbfb49f
Joseph Mazzei <j.mazzei.pv@gmail.com>
2024-02-25 17:05:20 +0000

    Added homepage view of all database posts, changed url paths. Deployment push

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-jomazzei-djangoblog-4bh3vv95vab.ws-eu108.gitpod.io','.herokuapp.com']
 
