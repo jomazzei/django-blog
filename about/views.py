88423589e9a217e3824286fb771bf0e48ba6fa58
Joseph Mazzei <j.mazzei.pv@gmail.com>
2024-02-27 15:12:27 +0000

    views and about template created

--- about/views.py
+++ about/views.py
@@ -5,12 +5,12 @@ from .models import About
 # Create your views here.
 def about_me(request):
     """
-    Renders About page
+    Renders the About page
     """
     about = About.objects.all().order_by('-updated_on').first()
-    
+
     return render(
-    request,
-    "about/about.html",
-    {"about": about},
+        request,
+        "about/about.html",
+        {"about": about},
     )
