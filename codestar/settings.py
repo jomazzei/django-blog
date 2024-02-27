c3e81c1a93c5ac6d685aa7db831eda193c28dead
Joseph Mazzei <j.mazzei.pv@gmail.com>
2024-02-27 15:47:16 +0000

    Deployment push

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-jomazzei-djangoblog-4bh3vv95vab.ws-eu108.gitpod.io','.herokuapp.com']
 
