675de4970f159fd643eace56243be879fce3e497
Joseph Mazzei <j.mazzei.pv@gmail.com>
2024-03-01 13:38:52 +0000

    Transfer init commit, making sure all workspaces are synced and working before continueing

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-jomazzei-djangoblog-4bh3vv95vab.ws-eu108.gitpod.io','.herokuapp.com']
+ALLOWED_HOSTS = ['.gitpod.io','.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
